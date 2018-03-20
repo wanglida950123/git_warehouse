ed9f69429c98bb0b4509285cb77fab5b8f198222
wanglida950123 <wanglida0123@163.com>
2018-03-20 10:42:27 +0800

    updata

--- langzhuo_demo/github_demo/src/main/java/github_demo/demo/service/UserLogin.java
+++ langzhuo_demo/github_demo/src/main/java/github_demo/demo/service/UserLogin.java
@@ -1,9 +1,13 @@
 package github_demo.demo.service;
 
+import org.springframework.ui.Model;
+
 import github_demo.demo.entity.User;
 
 public interface UserLogin {
 	
 	User queryByUid(String uid);
+	
+	String loginValidata(Model model, String username, String password);
 
 }
--- langzhuo_demo/github_demo/src/main/java/github_demo/demo/service/imp/UserLoginImp.java
+++ langzhuo_demo/github_demo/src/main/java/github_demo/demo/service/imp/UserLoginImp.java
@@ -1,7 +1,11 @@
 package github_demo.demo.service.imp;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import github_demo.demo.dao.UserMapper;
 import github_demo.demo.entity.User;
@@ -15,6 +19,33 @@ public class UserLoginImp implements UserLogin {
 	
 	public User queryByUid(String uid) {
 		return userMapper.selectByPrimaryKey(uid);
+	}
+
+	public String loginValidata(Model model, String username, String password) {
+		String result = "";
+		User user = userMapper.selectByPrimaryKey(username);
+		if(user == null){
+			result = "该用户不存在";
+			model.addAttribute("result", result);
+		}else{
+			try {
+				if(password != null && password.equals(user.getMm())){
+					if(InetAddress.getLocalHost().getHostAddress() == user.getIp()){
+						result = "/ks/list";
+					}else{
+						result = "IP地址错误";
+						model.addAttribute("result", result);
+					}
+				}else{
+					result = "密码输入错误";
+					model.addAttribute("result", result);
+				}
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
 
 
